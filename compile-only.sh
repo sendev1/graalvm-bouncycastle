58e71d63e4d37ad67fd32e87a8c58cb3c59e8e07
Kris Foster <me@krismade.me>
2021-02-16 10:58:01 +0000

    Works - added list of fixes

---
+++ compile-only.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+ARTIFACT=boot-2.3.1
+MAINCLASS=com.demo.DemoApplication
+VERSION=0.0.1-SNAPSHOT
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+mkdir -p build/native-image
+
+JAR="$ARTIFACT-$VERSION.war"
+rm -f $ARTIFACT
+echo "Unpacking $JAR"
+cd build/native-image
+jar -xvf ../libs/$JAR >/dev/null 2>&1
+cp -R META-INF WEB-INF/classes
+
+# To run tracing agent
+# java -cp WEB-INF/classes:WEB-INF/lib/*:WEB-INF/lib-provided/* -agentlib:native-image-agent=config-output-dir=../graal-agent com.vue.labs.LocalAbeServiceApplication
+
+LIBPATH=`find WEB-INF/lib | tr '\n' ':'`
+CP=WEB-INF/classes:$LIBPATH
+
+GRAALVM_VERSION=`native-image --version`
+echo "Compiling $ARTIFACT with $GRAALVM_VERSION"
+native-image \
+  --no-server \
+  --no-fallback \
+  --enable-all-security-services \
+  --verbose \
+  -H:Name=$ARTIFACT \
+  -H:+RemoveSaturatedTypeFlows \
+  -H:+ReportExceptionStackTraces \
+  -H:+PrintClassInitialization \
+  --rerun-class-initialization-at-runtime=org.bouncycastle.jcajce.provider.drbg.DRBG\$Default,org.bouncycastle.jcajce.provider.drbg.DRBG\$NonceAndIV \
+  -Dspring.native.remove-yaml-support=true \
+  -cp $CP $MAINCLASS
+
+#   --rerun-class-initialization-at-runtime=org.bouncycastle.jcajce.provider.drbg.DRBG$Default,org.bouncycastle.jcajce.provider.drbg.DRBG$NonceAndIV \
+#  --trace-class-initialization=org.bouncycastle.jcajce.provider.drbg.DRBG \
+#  --initialize-at-run-time=org.bouncycastle.jcajce.provider.drbg.DRBG \
